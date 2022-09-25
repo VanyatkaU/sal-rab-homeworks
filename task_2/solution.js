d9c74ddd34bd721089edc3eaa607f70ea22024ba
VanyatkaU <88547613+VanyatkaU@users.noreply.github.com>
2022-09-25 21:18:54 +0300

    Update solution.js

--- task_2/solution.js
+++ task_2/solution.js
@@ -21,7 +21,7 @@ function calcShipping(sum, min, shipping) {
     
     if (productsSum == 0 || productsSum >= freeShippingMinSum) {
         shippingSum = 0;
-    } else {
+    } else if (productsSum >= 0 && productsSum < freeShippingMinSum) {
         shippingSum = shippingPrice;
     }
 
